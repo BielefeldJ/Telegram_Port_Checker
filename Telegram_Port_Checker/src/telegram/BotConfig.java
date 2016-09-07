eb82c9f46319b2db92100c618a5e90ea9760c296
TheJens90 <TheJens90@users.noreply.github.com>
2016-09-07 17:34:48 +0200

    Delete BotConfig.java

--- Telegram_Port_Checker/src/telegram/BotConfig.java
+++
@@ -1,7 +0,0 @@
-package telegram;
-
-public class BotConfig 
-{
-    public static final String BOT_USERNAME = "BielefeidlServer_TSBot";
-    public static final String BOT_TOKEN = "228020017:AAHWYE10-7sd_dBl9DIRtf0sYmjyBaqQyNQ";
-}
